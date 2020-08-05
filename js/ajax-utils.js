b68fa0b5d5183733b1cf4fb8f7f3762e674851de
akshay4262 <51784375+akshay4262@users.noreply.github.com>
2020-08-05 13:03:59 +0530

    Minor changes

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -1,16 +1,13 @@
 (function (global) {
 
-// Set up a namespace for our utility
 var ajaxUtils = {};
 
 
-// Returns an HTTP request object
 function getRequestObject() {
   if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
   else if (window.ActiveXObject) {
-    // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
   else {
@@ -19,8 +16,6 @@ function getRequestObject() {
   }
 }
 
-
-// Makes an Ajax GET request to 'requestUrl'
 ajaxUtils.sendGetRequest = 
   function(requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
@@ -31,20 +26,16 @@ ajaxUtils.sendGetRequest =
                        isJsonResponse); 
       };
     request.open("GET", requestUrl, true);
-    request.send(null); // for POST only
+    request.send(null);
   };
 
 
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
 function handleResponse(request,
                         responseHandler,
                         isJsonResponse) {
   if ((request.readyState == 4) &&
      (request.status == 200)) {
 
-    // Default to isJsonResponse = true
     if (isJsonResponse == undefined) {
       isJsonResponse = true;
     }
@@ -58,8 +49,6 @@ function handleResponse(request,
   }
 }
 
-
-// Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
 
 
